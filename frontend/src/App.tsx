4b085f085920c92a9ebb2656464a4f8d2dbe2d64
okakyo <0622okakyo@gmail.com>
2019-06-14 16:31:07 +0900

    提出

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router,Link,Route} from 'react-router-dom';
-
-//@types/react-router-dom を追加
-
-
 
 const App: React.FC = () => {
   return (
