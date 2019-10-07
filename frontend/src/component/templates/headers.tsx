9dc3a3209ff076f8c9f8fae0860f9e466db03dc5
okakyo <0622okakyo@gmail.com>
2019-10-07 16:35:36 +0900

    フォルダ設計中

---
+++ frontend/src/component/templates/headers.tsx
@@ -0,0 +1,19 @@
+
+import React from "react";
+
+
+import * as Route from "react-router";
+import * as router from "react-router-dom";
+
+
+class Headers extends React.Component{
+    constructor(props:any) {
+        super(props);
+    }
+
+
+    render(){
+        return {}
+    }
+
+};
