662689f4489d4facff4af25c21b3b2ccc47956ea
okakyo <0622okakyo@gmail.com>
2019-11-12 15:51:54 +0900

    細かい要件を定義

--- frontend/.storybook/config.js
+++ frontend/.storybook/config.js
@@ -1,8 +1,9 @@
 import { configure } from '@storybook/react';
 
 function loadStories() {
-    require('./story/index.js');
-    // You can require as many stories as you need.
+    require('./story/atoms.js');
+    require('./story/templates.js');
+
 }
 
 configure(loadStories, module);
